ea1628b4ace1581936e7cf74ed97c1913d0c0c76
bua <buaisaac17@gail.com>
2023-07-26 12:50:08 -0700

    created the main.h file and added the header files and also created the printf file and addedt the scripts

---
+++ _printf.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * _printf - is a custom printf function in C
+ * @format: format variable string for the function
+ * Return: the str, char or values input
+ */
+
+int _printf(const char *format, ...)
+{
+	int print = 0;
+
+	va_list args;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			print++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+			{
+				break;
+			}
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				print++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+				int strlen = 0;
+
+				while (str[strlen] != '\0')
+				{
+					strlen++;
+				}
+				write(1, str, strlen);
+				print += strlen;
+			}
+			if (*format == 'c')
+			{
+				char notst = (char)va_arg(args, int);
+
+				write(1, &notst, 1);
+				print++;
+			}
+		}
+
+		format++;
+	}
+	va_end(args);
+	return (print);
+}
